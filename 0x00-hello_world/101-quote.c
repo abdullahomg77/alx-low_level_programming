71ca06aea5a02a14506cb73a825f458574a7d709
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-14 19:00:46 -0700

    88

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
+#include<unistd.h>
+
 /**
  * main - Entry
  * Decription: print use write
  * Return: always 1
 */
+
 int main(void)
 {	char abc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(1, abc, 59);
 	return (1);
-
-
 }
