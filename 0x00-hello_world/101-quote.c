942d8c828e5466e133851da66d72f8735e7ce8bf
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-14 18:55:24 -0700

    daa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
  * main - Entry
  * Decription: print use write
  * Return: always 1
- * */
+*/
 int main(void)
-{
-	char abc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+{	char abc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write(1,abc.59);
+	write(1, abc, 59);
 	return (1);
 
 
