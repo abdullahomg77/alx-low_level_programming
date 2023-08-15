433074904d19566abbb429d408fa5838a464a554
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-14 18:48:32 -0700

    7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unstd.h>
+/**
+ * main - Entry
+ * Decription: print use write
+ * Return: always 1
+ * */
+int main(void)
+{
+	char abc[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	
+	write(1,abc.59);
+	return (1);
+
+
+}
