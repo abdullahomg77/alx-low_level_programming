5eeea207f7a9883d676f67973da9bd2a2aff24ef
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-17 07:25:56 -0700

    dd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
 */
 
@@ -11,6 +11,6 @@ int main(void)
 
 	i = 98 % 2;
 	positive_or_negative(i);
-	
+
 return (0);
 }
