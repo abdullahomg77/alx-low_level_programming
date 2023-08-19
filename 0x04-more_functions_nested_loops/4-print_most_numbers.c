90683eb247dca9b248ef20eb22301265d4c15ff8
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-18 18:58:45 -0700

    ssss

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all number except 2 and 4
+ * Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	do {
+		_putchar(i + 48);
+		i++
+	} while (i <= 0 && i != 2 && i != 4 && i >= 9);
+	_putchar('\n');
+}
