da0f3abf0f634800d743a0c8a22d51c6577817d8
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-21 09:02:02 -0700

    set

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,11 +11,11 @@ void print_line(int n)
 	int line;
 
 	if (n <= 0)
-		_putchar("\n");
+		_putchar('\n');
 	else
 	{
 		for (line = 1; line <= n; line++)
-			_putchar("_");
-		_putchar("\n");
+			_putchar('_');
+		_putchar('\n');
 	}
 }
