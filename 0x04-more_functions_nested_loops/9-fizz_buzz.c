4b4e503878b0614540503b7612d3799f7ee305cb
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-21 11:07:26 -0700

    ahos1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
 			printf("Fizz");
@@ -19,6 +19,7 @@ int main(void)
 			printf("FizzBuzz");
 		else
 			printf("%d", i);
+
 		if (i != 100)
 			printf(" ");
 		else
