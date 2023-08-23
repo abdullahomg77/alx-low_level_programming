e9c252788b2916ae891256b12b3a4e74c8bbbb22
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-22 19:17:33 -0700

    aw

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse string
+ * @s: string
+ * Return: none
+*/
+
+void rev_string(char *s)
+{
+	int l = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		l++;
+	}
+	t = l -1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+	}
+	putchar('\n');
+}
