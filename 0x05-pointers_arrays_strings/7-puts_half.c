1d1fc5d86be2a192e8a60f4585e1089134033435
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-22 19:42:44 -0700

    wt

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half fo a string
+ * @str: strin para
+ * Return: none
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	purchar('\n');
+}
