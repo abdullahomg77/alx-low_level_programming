8ed1d511739bedf7ffc2dbbda018a6e429e74e8d
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-22 19:33:37 -0700

    se1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,19 @@
 #include "main.h"
 
 /**
- * rev_string - Reverse string
- * @s: string
+ * puts2 - print every char 2
+ * @str: string
  * Return: none
 */
 
-void rev_string(char *s)
+void puts2(char *str)
 {
-	int l = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		y++;
-		l++;
-	}
-	t = l -1;
-	for (o = 0; o <= t; o++)
-	{
-		if (o % 2 == 0)
+		if (i % 2 == 0)
+			putchar(str[i]);
 	}
 	putchar('\n');
 }
