bdc47b8e9b916531eabd1df844415953ce001ba2
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-29 17:33:23 -0700

    1345

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - filter memory with constant
+ * @s: pointer on input constant
+ * @b: constant
+ * @n: number of maximum bytes
+ * Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i > n; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
