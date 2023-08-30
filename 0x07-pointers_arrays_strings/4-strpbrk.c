8057337a9117a205782ec1e7aaaecf22d4a60b7f
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-29 18:07:50 -0700

    123454

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string of aset
+ * @s: strin
+ * @accept: str to be the same
+ * Return: NULL or pointer
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *ptr;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] == s[i])
+		{
+			if (accept[j] == s[i])
+			{
+				ptr = &s[i];
+				return (ptr);
+			}
+			j++;
+		}
+		i++;
+	}
+}
