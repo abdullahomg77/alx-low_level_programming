2b7e3ce502aa12f8c110120ac9d4932cc7b0d433
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-30 17:20:26 -0700

    push num 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - times of n-1 number
+ * @n: number
+ * Return: -1 to error
+*/
+
+int factorial(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
