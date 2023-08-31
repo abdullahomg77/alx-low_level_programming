a57e623cd18a9f2b15d65b9bb22a4984ea30d1cc
abdullahomg77 <abdullahomg77@gmail.com>
2023-08-30 17:40:23 -0700

    push num 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise num to the power of num2
+ * @x: number
+ * @y: raised number
+ * Return: for error -1
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
