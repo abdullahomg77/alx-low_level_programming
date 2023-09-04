847d392e9d4d64a1bd4673a4e7df1240399bb18d
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-03 18:21:33 -0700

    12

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print sum of 2 integer
+ * @argc: count
+ * @argv: array
+ * Return: 0, or 1 if fail
+*/
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc == 3)
+	{
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", n);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
