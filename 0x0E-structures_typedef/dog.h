4a04c6bcfbbdd6d72735937090417093a187f2b0
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-12 18:28:40 -0700

    2

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Description: lost dog and have no one
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
