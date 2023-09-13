56b1605218476c5343d7e6a63c1e3f84fcc5f88b
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-12 18:45:36 -0700

    123

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog  - free
+ * @d: yo dawg
+ * Return: none
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
