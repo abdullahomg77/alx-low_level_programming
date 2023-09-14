57fff0d7875f62b59de8b1658fa4c47dcd92fb7a
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-13 20:02:48 -0700

    11

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,13 +16,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int n = 0;
+	int i = 0;
 
-	while (n < 5)
-	{
-		if (s && s[0] == ops[n].op[0] && !s[1])
-			return (ops[n].f);
-		n++;
-	}
-	return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
 }
