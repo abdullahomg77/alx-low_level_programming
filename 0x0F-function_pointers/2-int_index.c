531cef59228d56982441960686e8971eb0467316
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-13 18:33:45 -0700

    123

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointer.h"
+
+/**
+ * int_index - search for int
+ * @array: ints
+ * @size: aray size
+ * @cmp: compare function
+ * Return: the int index
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
