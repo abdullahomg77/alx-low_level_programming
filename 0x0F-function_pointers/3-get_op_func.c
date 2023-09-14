af5d109b8a9986ec856cf6d70b6d43358e75d955
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-13 19:25:12 -0700

    1

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - return pointer
+ * @s: string
+ * Return: pointer
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int n = 0;
+
+	while (n < 5)
+	{
+		if (s && s[0] == ops[n].op[0] && !s[1])
+			return (ops[n].f);
+		n++
+	}
+	return (NULL);
+}
