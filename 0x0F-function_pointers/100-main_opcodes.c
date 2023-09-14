30a62c1e85c1bf3276f5602972d090732edf06e6
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-13 19:41:15 -0700

    11

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print own opcodes
+ * @argc: count
+ * @argv: val
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, n;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (n = 0; n < bytes; n++)
+	{
+		if (n == bytes - 1)
+		{
+			printf("%02hhx\n", arr[n]);
+			break;
+		}
+		printf("%02hhx ", arr[n]);
+	}
+	return (0);
+}
