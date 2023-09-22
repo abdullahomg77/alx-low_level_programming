041480d2d96a6692072fb204f91a8d7ccf9f9792
abdullahomg77 <abdullahomg77@gmail.com>
2023-09-21 19:20:34 -0700

    w

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,16 +6,16 @@
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
-*/
+ */
 
 size_t list_len(const list_t *h)
 {
-	size_t s = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		s++;
-		s = h->next;
+		n++;
+		h = h->next;
 	}
-	return (s);
+	return (n);
 }
