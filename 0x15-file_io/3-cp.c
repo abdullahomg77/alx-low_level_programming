16a7356f151f2eff128c3ec9069e729f0c3b63b4
abdullahomg77 <abdullahomg77@gmail.com>
2023-10-02 18:55:17 -0700

    1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		n2 = write(file_to, buf, num1);
+		n2 = write(file_to, buf, n1);
 		if (n2 < n1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
