8dd5762d9be40c5d2c51e27c09b723ea4ac0a680
abdullahomg77 <abdullahomg77@gmail.com>
2024-01-01 23:38:25 -0800

    1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/abdullahomg77/alx-low_level_programming/raw/master/0x018-dynamic_libaries/libgiga.so
+wget -P /tmp https://github.com/abdullahomg77/alx-low_level_programming/raw/master/0x18-dynamic_libaries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
